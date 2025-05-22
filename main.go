5a249dec10d22f8a0488eb0c09241e04c6df8ca0
kotjuz <rkotarski10@gmail.com>
2025-05-22 17:04:42 +0200

    fix: fixed downloading from url

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"example.com/downloader/filemanager"
 )
@@ -12,7 +17,44 @@ const (
 )
 
 func main() {
-	fm := filemanager.New(inputFilePath, "./res.txt")
+	fm := filemanager.New(inputFilePath, "downloaded/")
 	lines, _ := fm.ReadFile()
 	fmt.Println(lines)
+
+	fm.CreateEmptyDir()
+
+	for _, url := range lines {
+		downloadFromUrl(url)
+	}
+}
+
+func downloadFromUrl(url string) error {
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	defer response.Body.Close()
+
+	parts := strings.Split(url, "/")
+	filename := parts[len(parts)-1]
+
+	filepath := filepath.Join("downloaded/", filename)
+	fmt.Println(filepath)
+	out, err := os.Create(filepath)
+
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	defer out.Close()
+
+	_, err = io.Copy(out, response.Body)
+
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return nil
 }
